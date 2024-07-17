3c141fea931979354921584e7256166e5e66de2d
Joseph Boulos <jab12288@gmail.com>
2024-07-17 11:02:16 -0700

    add about page and simple routing

---
+++ app/about/page.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export const AboutPage = () => {
+  return (
+    <main>
+      <h1>About us</h1>
+      <p>
+        <a href="/">Homepage</a>
+      </p>
+    </main>
+  )
+}
+
+export default AboutPage
--- app/page.js
+++ app/page.js
@@ -4,6 +4,9 @@ export default function Home() {
       <img src="/logo.png" alt="Logo" />
       <h1>Recipe Realm</h1>
       <p>🔥 Let&apos;s get cookin! 🔥</p>
+      <p>
+        <a href="/about">About Us</a>
+      </p>
     </main>
   )
 }
